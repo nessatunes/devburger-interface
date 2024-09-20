e03f3239465cbfc8bdc227602286c903038b28bb
Nessatunes <essatunes@gmail.com>
2024-09-20 19:14:02 -0300

    feat: add menu categorias end produtos

--- src/containers/Menu/index.jsx
+++ src/containers/Menu/index.jsx
@@ -1,6 +1,39 @@
-import { Container, Banner, CategoryMenu, ProductsContainer } from './styles';
+import { useState, useEffect } from 'react';
+import {
+  Container,
+  Banner,
+  CategoryMenu,
+  ProductsContainer,
+  CategoryButton,
+} from './styles';
+import { api } from '../../services/api';
+import { formatPrice } from '../../utils/formatPrice';
+import { CardProduct } from '../../components/CardProduct';
 
 export function Menu() {
+  const [categories, setCategories] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function loadCategories() {
+      const { data } = await api.get('/categories');
+      const newCategories = [{ id: 0, name: 'Todas' }, ...data];
+      setCategories(newCategories);
+    }
+
+    async function loadProducts() {
+      const { data } = await api.get('/products');
+      const newProducts = data.map((product) => ({
+        currencyValue: formatPrice(product.price),
+        ...product,
+      }));
+
+      setProducts(newProducts);
+    }
+    loadCategories();
+    loadProducts();
+  }, []);
+
   return (
     <main>
       <Container>
@@ -13,9 +46,15 @@ export function Menu() {
           </h1>
         </Banner>
         <CategoryMenu>
-          <h1>Seja Bem Vindo!</h1>
+          {categories.map((category) => (
+            <CategoryButton key={category.id}>{category.name}</CategoryButton>
+          ))}
         </CategoryMenu>
-        <ProductsContainer></ProductsContainer>
+        <ProductsContainer>
+          {products.map((product) => (
+            <CardProduct product={product} key={product.id} />
+          ))}
+        </ProductsContainer>
       </Container>
     </main>
   );
