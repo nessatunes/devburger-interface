06cf4375d6f3667570d043e51e8f5cdbd85aeef6
Nessatunes <essatunes@gmail.com>
2024-09-20 22:50:08 -0300

    feat: add useProvider

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import { ToastContainer } from 'react-toastify';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
 import GlobalStyles from './styles/globalStyles';
+import AppProvider from './hooks';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
-    <GlobalStyles />
-    <ToastContainer autoClose={3000} theme="colored" />
+    <AppProvider>
+      <RouterProvider router={router} />
+      <GlobalStyles />
+      <ToastContainer autoClose={3000} theme="colored" />
+    </AppProvider>
   </StrictMode>,
 );
