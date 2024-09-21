ffccf7a7ba674770325c0fea2e9b9639d7075a3d
Nessatunes <essatunes@gmail.com>
2024-09-20 23:08:21 -0300

    chore: add inicial header

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,12 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
 import GlobalStyles from './styles/globalStyles';
 import AppProvider from './hooks';
+import Header from './components/Header';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AppProvider>
+      <Header />
       <RouterProvider router={router} />
       <GlobalStyles />
       <ToastContainer autoClose={3000} theme="colored" />
