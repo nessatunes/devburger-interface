f45d15676b7b8d70bdf73667b17750a5c7afdbae
Nessatunes <essatunes@gmail.com>
2024-10-26 23:10:19 -0300

    add

--- src/containers/Checkout/index.jsx
+++ src/containers/Checkout/index.jsx
@@ -1,7 +1,7 @@
 import { Elements } from '@stripe/react-stripe-js';
 import { useLocation } from 'react-router-dom';
 import stripePromise from '../../config/stripeConfig';
-import  CheckoutForm  from '../../components/Stripe/CheckoutForm';
+import CheckoutForm from '../../components/Stripe/CheckoutForm';
 
 export function Checkout() {
   const {
